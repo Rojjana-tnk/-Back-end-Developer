11f5a6350c6910b4c58fe3a08cc5baae02535a6a
Rojjana-tnk <67319010050@tnk.ac.th>
2024-10-29 13:55:32 +0700

    Paper Scissors Game

---
+++ python_week03/test01.py
@@ -0,0 +1,16 @@
+import random
+print("โปรแกรมเป่ายิงฉุบ")
+while True:
+    a = random.choice(["ค้อน","กรรไกร","กระดาษ"])
+    print(a)
+    b = str(input("คำตอบ :"))
+    if b == a:
+        print("เสมอ")
+    elif a =="กรรไกร" and b == "ค้อน":
+        print("คุณชนะ")
+    elif a =="กระดาษ" and b == "กรรไกร":
+        print("คุณชนะ")    
+    elif a =="ค้อน" and b == "กระดาษ":
+        print("คุณชนะ")
+    elif:
+        print("คุณแพ้")
